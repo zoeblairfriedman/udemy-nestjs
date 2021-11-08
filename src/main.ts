7b485852f439e078ef925f83cd839a3de49320e8
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-11-08 13:23:39 -0500

    adds first module and bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 // first file executed in any nest project
 import { Controller, Module, Get} from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
 
 // this is a decorator. we are creating a class that will be a controller. it will handle and route incoming requests.
 @Controller()
@@ -9,3 +10,22 @@ class AppController {
     return 'hi there';
   }
 }
+
+//create a module about the app controller 
+@Module({
+  controllers: [AppController]
+})
+
+
+class AppModule {}
+
+//define and invoke the bootstrap function, create an instance of our application and listen for incoming traffic
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+
+  await app.listen(3000)
+}
+
+bootstrap();
+
+// use this: npx ts-node-dev src/main.ts cause we built it from scratch
